92c540a74559640d97d5ae7b9c8dcc140116b356
Shivesh <shiveshs98@gmail.com>
2019-09-01 19:05:17 +0530

    Updates

---
+++ precache-manifest.94999a9e67d356153ba2cce8016e3861.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536a1260f7743db6726654d923f1f0fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33195642b19194727c87",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "38164835c7b519f290bb",
+    "url": "/static/css/main.9b71dae7.chunk.css"
+  },
+  {
+    "revision": "33195642b19194727c87",
+    "url": "/static/js/2.f9039d39.chunk.js"
+  },
+  {
+    "revision": "38164835c7b519f290bb",
+    "url": "/static/js/main.c41cdb52.chunk.js"
+  },
+  {
+    "revision": "7c23a43b52e067a1c7c0",
+    "url": "/static/js/runtime~main.f6dce284.js"
+  },
+  {
+    "revision": "b9449715755c6ce116c9acfe2578a51a",
+    "url": "/static/media/acm.b9449715.webp"
+  },
+  {
+    "revision": "5d6a737a78ac4fc0040ad387280bd79d",
+    "url": "/static/media/leo.5d6a737a.webp"
+  },
+  {
+    "revision": "1ec786aaa8c7a86af6ce0d47cefa524b",
+    "url": "/static/media/pcp.1ec786aa.webp"
+  },
+  {
+    "revision": "1e51e6747d83f6b13fdb3199c1cf4844",
+    "url": "/static/media/profile.1e51e674.webp"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
